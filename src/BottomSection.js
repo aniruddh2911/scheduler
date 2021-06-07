b519c0023e7c7667773feaceeb82482f416b4a4d
aniruddh2911 <aniruddh2911@gmail.com>
2021-06-07 14:47:36 +0530

    Task-4

---
+++ src/BottomSection.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function BottomSection() {
+    return (
+        <div className="section__4">
+            <h1>hello</h1>
+        </div>
+    )
+}
+
+export default BottomSection;
---
+++ src/RightSection.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import EditIcon from '@material-ui/icons/Edit';
+import './RightSection.css';
+
+function RightSection() {
+    return (
+        <div>
+            <div className="section__2">
+                <h1 className="h1__section2">Batch Name|PHY_B1</h1>
+                <EditIcon />
+            </div>
+        </div>
+    )
+}
+
+export default RightSection
